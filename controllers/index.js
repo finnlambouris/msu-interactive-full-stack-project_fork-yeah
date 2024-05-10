90a7b6dcbb1ca166ea6f920a0b7983377f18341b
finntendoverse <finnlambouris616@gmail.com>
2024-05-09 20:49:01 -0400

    refactored login routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
-const loginRoutes = require('./loginRoutes.js');
 const apiRoutes = require('./api');
 
+const signupRoutes = require('./signupRoutes.js');
+const loginRoutes = require('./loginRoutes.js');
+const logoutRoutes = require('./logoutRoutes.js');
+
 // Define your routes here
-router.use('/', loginRoutes);
 router.use('/api', apiRoutes);
+router.use('/signup', signupRoutes);
+router.use('/login', loginRoutes);
+router.use('/logout', logoutRoutes);
 
 router.get('/', (req, res) => {
   res.send('Hello World');
