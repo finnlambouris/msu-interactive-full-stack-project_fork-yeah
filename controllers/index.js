4bffe7a20c1d96fd378088105543085a0001a2e2
peyton brimmer <peytonbrimmer0@gmail.com>
2024-05-09 20:34:38 -0400

    posts small commit index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 // controllers/index.js
 const express = require('express');
 const router = express.Router();
+const indexController = require('./controllers/index.js');
+const indexRouter = require('./controllers/index.js');
 
 // Define your routes here
 router.get('/', (req, res) => {
@@ -8,3 +10,36 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+  // Create a new post 
+    // req.body contains the post data
+    // Save the post to the database
+router.post('/posts', (req, res) => {
+    res.send('Post created successfully');
+});
+
+// Get all posts from the database
+router.get('/posts', (req, res) => {
+    res.send('List of all posts');
+});
+
+// Get a single post by id
+router.get('/posts/:id', (req, res) => {
+    res.send('Post details');
+});
+
+    // Update a post by id
+    // req.body contains the updated post data
+    // Update the post in the database
+router.put('/posts/:id', (req, res) => {
+    res.send('Post updated successfully');
+});
+
+// Delete a post by id
+router.delete('/posts/:id', (req, res) => {
+    res.send('Post deleted successfully');
+});
+
+
+
+
