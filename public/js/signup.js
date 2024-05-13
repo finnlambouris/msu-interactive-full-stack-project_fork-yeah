c9014650d1cdbd0dbd5803bbf0251082debab5f6
finntendoverse <finnlambouris616@gmail.com>
2024-05-13 18:47:30 -0400

    front end log in and sign up functionality

---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+async function signup(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#signup-username').value;
+    const password = document.querySelector('#signup-password').value;
+  
+    const response = await fetch(`/signup`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Account creation failed. Please try again.');
+    }
+  }
+
+const signupForm = document.querySelector('#signup')
+signupForm.addEventListener('submit', signup);
